cad780046658c589a65ef31e5a74ebdaff9d24e1
samsonhodges2024 <93276396+samsonhodges2024@users.noreply.github.com>
2023-12-06 20:57:20 -0500

    Update listShows.js

--- listShows.js
+++ listShows.js
@@ -1,24 +1,47 @@
-import { get } from "./api"
+import { post } from "./api"
 
 export function getShows() {
-    // this sends the ACTUAL POST and retrieves the answer.
-    get('/listShows')
-        .then(function (response) {
-            console.log(response.constants)
-            let str = ''
-            for (let show of response.constants) {
-                str += show.show_name + '<br>'
-            }
+    //venue data
+    let credential = document.getElementById("passwordListShows")
 
-            // insert HTML in the <div> with 
-            // constant-list
-            <div id='show-list'></div>
-            let cd = document.getElementById('show-list')
-            cd.innerHTML = str
-
-        })
-        .catch(function (error) {
-            // not much to do
-            console.log(error)
-        })
+    // prepare payload for the post
+    let data = { 
+        'passwordListVenues': credential.value
     }
+
+    const handler = (json) => {
+        console.log(json)
+        credential.value = ''
+        document.getElementById("result").value = json.body
+    }
+
+    post('/listShows', data, handler)
+  
+}
+
+
+
+// import { get } from "./api"
+
+// export function getShows() {
+//     // this sends the ACTUAL POST and retrieves the answer.
+//     get('/listShows')
+//         .then(function (response) {
+//             console.log(response.constants)
+//             let str = ''
+//             for (let show of response.constants) {
+//                 str += show.idshow + "  " + show.show_name + " " + show.datetime + " " + show.price_type + "  " + show.is_active + '<br>';
+//             }
+
+//             // insert HTML in the <div> with 
+//             // constant-list
+//             <div id='show-list'></div>
+//             let cd = document.getElementById('show-list')
+//             cd.innerHTML = str
+
+//         })
+//         .catch(function (error) {
+//             // not much to do
+//             console.log(error)
+//         })
+//     }
