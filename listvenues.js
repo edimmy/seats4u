2ffd86d41db9c940940340f2f71089baa382bfa0
samsonhodges2024 <93276396+samsonhodges2024@users.noreply.github.com>
2023-12-07 14:00:28 -0500

    Update listvenues.js

--- listvenues.js
+++ listvenues.js
@@ -1,51 +1,29 @@
-import { post } from "./api"
+import { get } from "./api"
 
 export function getVenues() {
-    //venue data
-    let credential = document.getElementById("passwordListVenues")
+    // this sends the ACTUAL POST and retrieves the answer.
+    get('/listVenues')
+        .then(function (response) {
+            console.log(response.constants)
+            let str = ''
+            for (let venue of response.constants) {
+                str += venue.name + " " + venue.right + " " + venue.rnum_rows + " " + venue.rseats_in_row + " " +
+                venue.center + " " + venue.cnum_rows + " " + venue.cseats_in_row + " " +
+                venue.lenter + " " + venue.lnum_rows + " " + venue.lseats_in_row + '<br>';
 
+            }
 
-    // prepare payload for the post
-    let data = { 
-        'passwordListVenues': credential.value
+            // insert HTML in the <div> with 
+            // constant-list
+            <div id='venue-list'></div>
+            let cd = document.getElementById('venue-list')
+            cd.innerHTML = str
+
+        })
+        .catch(function (error) {
+            let sample = error
+            // not much to do
+            console.log(error)
+        })
     }
 
-    const handler = (json) => {
-        console.log(json)
-        credential.value = ''
-        document.getElementById("result").value = json.body
-
-    }
-
-    post('/listVenues', data, handler)
-  
-}
-
-
-// import { get } from "./api"
-
-// export function getVenues() {
-//     // this sends the ACTUAL POST and retrieves the answer.
-//     get('/listVenues')
-//         .then(function (response) {
-//             console.log(response.constants)
-//             let str = ''
-//             for (let venue of response.constants) {
-//                 str += venue.name + " " + venue.right + " " + venue.rnum_rows + " " + venue.rseats_in_row + " " +
-//                 venue.center + " " + venue.cnum_rows + " " + venue.cseats_in_row + " " +
-//                 venue.lenter + " " + venue.lnum_rows + " " + venue.lseats_in_row + '<br>';
-
-//             }
-
-//             // insert HTML in the <div> with 
-//             // constant-list
-//             <div id='venue-list'></div>
-//             let cd = document.getElementById('venue-list')
-//             cd.innerHTML = str
-
-//         })
-//         .catch(function (error) {
-//             // not much to do
-//             console.log(error)
-//         })
-//     }
