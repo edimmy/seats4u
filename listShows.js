615248d09979744dc3c8ffdf1a6c35e872405ca8
samsonhodges2024 <93276396+samsonhodges2024@users.noreply.github.com>
2023-12-08 17:14:09 -0500

    Update listShows.js

--- listShows.js
+++ listShows.js
@@ -6,7 +6,7 @@ export function getShows() {
 
     // prepare payload for the post
     let data = { 
-        'passwordListVenues': credential.value
+        'passwordListShows': credential.value
     }
 
     const handler = (json) => {
