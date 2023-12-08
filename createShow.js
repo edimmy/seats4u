d4b76005fa15db22f9b34fd61ca5368dfee931c0
samsonhodges2024 <93276396+samsonhodges2024@users.noreply.github.com>
2023-12-07 19:56:04 -0500

    Update createShow.js

--- createShow.js
+++ createShow.js
@@ -1,10 +1,12 @@
 import { post } from "./api"
+import { makeSeats } from "./makeSeats"
 
 export function createShow() {
     let venue_name = document.getElementById("venue_name")
     let show_name = document.getElementById("show_name")
     let datetime = document.getElementById("datetime")
     let price_type = document.getElementById("price_type")
+    let credential = document.getElementById("passwordCreateShow")
 
     let is_active = 0
     let is_sold_out = 0
@@ -16,7 +18,9 @@ export function createShow() {
         'is_active': is_active,
         'datetime': datetime.value, 
         'price_type': price_type.value, 
-        'is_sold_out': is_sold_out }
+        'is_sold_out': is_sold_out,
+        'passwordCreateShow': credential.value 
+    }
 
     
     const handler = (json) => {
@@ -26,8 +30,13 @@ export function createShow() {
         show_name.value = ''
         datetime.value = ''
         price_type.value = ''
+        credential.value = ''
     }
 
+    
+
     post('/createShow', data, handler)
+    
+    //makeSeats(credential, showID) ?????
   
 }
