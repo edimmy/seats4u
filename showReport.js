3d5b5c4ee9eb3dcddce3403a6bcbe7d8a1bdc9d5
samsonhodges2024 <93276396+samsonhodges2024@users.noreply.github.com>
2023-12-08 17:01:01 -0500

    Update showReport.js

--- showReport.js
+++ showReport.js
@@ -1,25 +1,27 @@
 import { get } from "./api"
 
-export function showAvailableSeats() {
+export function showReport() {
     // this sends the ACTUAL POST and retrieves the answer.
     get('/showReport')
         .then(function (response) {
-            console.log(response.constants)
-            let str = ''
-            for (let report of response.constants) {
-                // str += seats.num_rows0 + "  " + '<br>';
+            console.log(response.constants) 
 
+            let str = ''
+
+            for (let report of response.constants) {
+         
+                str += report.ShowName + ":  Seats Sold " + report.SeatsSold + ":  Seats Not Sold " + report.SeatsNotSold + ":  Proceeds " + report.Proceeds + "<br>"
             }
 
+          
             // insert HTML in the <div> with 
             // constant-list
-            <div id='report-list'></div>
-            let cd = document.getElementById('report-list')
+            <div id='showreport'></div>
+            let cd = document.getElementById("showreport")
             cd.innerHTML = str
 
         })
         .catch(function (error) {
             // not much to do
             console.log(error)
-        })
-    }
+        })}
