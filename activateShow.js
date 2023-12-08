00c33aee86502a69a34b0050706e61e1899c18df
samsonhodges2024 <93276396+samsonhodges2024@users.noreply.github.com>
2023-12-07 19:56:31 -0500

    Update activateShow.js

--- activateShow.js
+++ activateShow.js
@@ -1,22 +1,17 @@
 import { post } from "./api"
 
 export function activateShow() {
-    let venue_name = document.getElementById("venue_name_activate")
-    let show_name = document.getElementById("show_name_activate")
-    let is_active = 1
+    let show_name = document.getElementById("show_name_active")
 
     // prepare payload for the post
-    let data = { 
-        'venue_name': venue_name.value, 
-        'show_name': show_name.value,
-        'is_active': is_active 
+    let data = {
+        'show_name_active': show_name.value
      }
 
     
     const handler = (json) => {
         console.log(json)
         // clear inputs
-        venue_name.value = ''
         show_name.value = ''
     }
 
